d60d30b859b59e56116c567f7d43d61d5ba4c08d
Dawn Griffin <dawndegriffin@gmail.com>
2021-10-02 09:00:00 +0800

    vayu: RevengeOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vayu.mk
+    $(LOCAL_DIR)/revengeos_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-user \
-    lineage_vayu-userdebug \
-    lineage_vayu-eng
+    revengeos_vayu-user \
+    revengeos_vayu-userdebug \
+    revengeos_vayu-eng
--- revengeos_vayu.mk
+++ revengeos_vayu.mk
@@ -8,13 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+REVENGEOS_BUILDTYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-PRODUCT_NAME := lineage_vayu
+PRODUCT_NAME := revengeos_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
